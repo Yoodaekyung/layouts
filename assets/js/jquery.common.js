9517a3e6e3fc233c8707bc739cba881044c7a9f9
Yoodaekyung <eorud0818@naver.com>
2023-01-17 16:14:38 +0900

    first commit

---
+++ assets/js/jquery.common.js
@@ -0,0 +1,10 @@
+// 부드러운 스크롤
+new GambitSmoothScroll({ amount: 50, speed: 950, force: 3 });
+
+$(document).ready(function () {
+  try {
+
+  } catch (e) {
+    console.error('Error : ' + e.stack);
+  }
+});
